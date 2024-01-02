99dfc35422a46b71fac966401175f658e6733591
Nisarg Modi <124357940+Nisargmodi3803@users.noreply.github.com>
2024-01-02 22:24:24 +0530

    Hibernate

---
+++ Student_hibernateCfgXml/src/main/java/org/entities/Student.java
@@ -0,0 +1,34 @@
+package org.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student
+{
+    @Id
+    @GeneratedValue
+    @Column(name = "student_id")
+    private long id;
+
+    @Column(name="student_name")
+    private String studentName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
